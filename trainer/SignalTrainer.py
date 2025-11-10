b9f0b3990bb9e02658720cfe9008d7f055bc8a1f
Fabiola Pasini <fabiola.pasini@hotmail.it>
2025-11-10 17:33:38 +0100

    Feat(update README)

--- trainer/SignalTrainer.py
+++ trainer/SignalTrainer.py
@@ -4,7 +4,8 @@ import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 
-from networks.models import SimpleSignalNet
+from networks.SimpleSignalNet import SimpleSignalNet
+from networks.PeakDetectionModel import PeakDetectionModel
 
 
 # ============================================================
